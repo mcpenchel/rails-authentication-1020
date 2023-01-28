c3e0527ae0b9cb0b92be547390d00e33ba700716
Matheus Penchel <matheus.penchel@gmail.com>
2023-01-28 11:13:02 -0300

    Devise correctly set up

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# For Authentication
+gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # another possible action is sign_in
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name, :last_name, :address
+    ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name, :last_name, :address
+    ])
+  end
 end
